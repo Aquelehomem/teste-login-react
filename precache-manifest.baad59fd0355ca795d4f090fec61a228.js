84bf5b19bb623f9cca2494c518ec4dfac7c943f8
gustavo martins <gustavomartinsdp@outlook.com>
2020-06-28 22:15:40 -0300

    Updates

---
+++ precache-manifest.baad59fd0355ca795d4f090fec61a228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06024e1654bb158e8b2c8e827f9a3c9d",
+    "url": "/teste-login-react/index.html"
+  },
+  {
+    "revision": "2f40b96aafcc599243a3",
+    "url": "/teste-login-react/static/css/main.d43eb92a.chunk.css"
+  },
+  {
+    "revision": "e08942870eb43b271788",
+    "url": "/teste-login-react/static/js/2.34e92fa5.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/teste-login-react/static/js/2.34e92fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f40b96aafcc599243a3",
+    "url": "/teste-login-react/static/js/main.0fe57e87.chunk.js"
+  },
+  {
+    "revision": "e22fd8a6c059568272b4",
+    "url": "/teste-login-react/static/js/runtime-main.13d70df7.js"
+  }
+]);
